37f57e8e8a6d4d67606c878e453767b13102fb56
caiyikun <caiyikun@zhihu.com>
2025-07-02 20:12:58 +0800

    add 两数之和-哈希

--- go/code/两数之和-哈希/run.go
+++ go/code/两数之和-哈希/run.go
@@ -1,6 +1,18 @@
 package main
 
-func twoSum()
+func twoSum(src []int, target int) []int {
+	var result []int
+	hashMap := make(map[int64]int)
+	for index, item := range src {
+		aim := target - item
+		if val, find := hashMap[int64(aim)]; find {
+			result = append(result, index, val)
+		}
+		hashMap[int64(item)] = index
+	}
+
+	return result
+}
 
 func main() {
 
